9c5f02030f7036c415a3293cfbbf33671d8c6035
glauserc <claudedenis.glauser@post.ch>
2020-03-31 13:27:00 +0200

    custom marker in eigenem File

--- wwwroot/js/googlestaomap.js
+++ wwwroot/js/googlestaomap.js
@@ -3,6 +3,7 @@ import { GisService } from './gisservice.js';
 import { UiBridge } from './uibridge.js';
 import { getJson } from './httpclient.js';
 import { createIcon } from './posticons.js';
+import {InitCustomMarker, CustomMarker} from './custommarker.js';
 
 export class GoogleStaoMap {
 
@@ -116,6 +117,8 @@ export class GoogleStaoMap {
             map: this.map,
         });
         marker.addListener('click', () => this.markerClicked(poi));*/
+
+        /*
         var myLatlng = new google.maps.LatLng(poi.y,poi.x);
         const marker = new CustomMarker(
             myLatlng, 
@@ -124,9 +127,23 @@ export class GoogleStaoMap {
                 marker_id: '123',
                 htmlContent: 'your_markup_here',
                 color: '#DF1E1E',
-                        tooltip: 'marker tooltp'
+                tooltip: 'marker tooltp'
+            }
+        );*/
+
+        var myLatlng = new google.maps.LatLng(poi.y,poi.x);
+        const marker = new CustomMarker (
+            myLatlng, 
+            this.map,
+            {
+                marker_id: '123',
+                htmlContent: 'your_markup_here',
+                color: '#DF1E1E',
+                tooltip: 'marker tooltp'
             }
         );
+
+
         return marker;
     }
 
@@ -147,72 +164,3 @@ export class GoogleStaoMap {
 
 }
 
-
-
-function CustomMarker(latlng, map, args) {
-    this.latlng = latlng;
-    this.args = args;
-    this.setMap(map);
-}
-
-function InitCustomMarker() {
-
-    CustomMarker.prototype = new google.maps.OverlayView();
-
-    CustomMarker.prototype.draw = function () {
-
-        var self = this;
-
-        var div = this.div;
-
-        if (!div) {
-
-            div = this.div = document.createElement('div');
-
-            div.className = 'marker';
-
-            div.style.position = 'absolute';
-
-            //set the values passed in from the creation of the custom marker
-            div.innerHTML = this.args.htmlContent;
-            div.style.color = this.args.color;
-
-            if (typeof (self.args.marker_id) !== 'undefined') {
-                div.dataset.marker_id = self.args.marker_id;
-            }
-
-            //add events to the marker
-
-            google.maps.event.addDomListener(div, "mouseover", function (e) {
-                //do something on mouseover, maybe show some tooltip text
-            })
-
-            google.maps.event.addDomListener(div, "mouseout", function (e) {
-                //do something on mosueout, hide the tooltip text
-            })
-
-
-            var panes = this.getPanes();
-            panes.overlayImage.appendChild(div);
-        }
-
-        var point = this.getProjection().fromLatLngToDivPixel(this.latlng);
-
-        //position the custom marker on the map
-        if (point) {
-            div.style.left = (point.x) + 'px';
-            div.style.top = (point.y) + 'px';
-        }
-    };
-
-    CustomMarker.prototype.remove = function () {
-        if (this.div) {
-            this.div.parentNode.removeChild(this.div);
-            this.div = null;
-        }
-    };
-
-    CustomMarker.prototype.getPosition = function () {
-        return this.latlng;
-    }
-}
